d2ddd2030c1a1434d76e524891ee8fcfdd936c9a
Nguyen Anh Vu <nguyenanhvu@enjoyworks.co.kr>
2021-12-15 14:35:20 +0700

    add useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,44 @@
-import './App.css';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Main } from './components/Main';
-import Person from './components/Person';
-import { PersonList } from './components/PersonList';
-import { LoggedIn } from './components/StateComponents/LoggedIn';
-import { Status } from './components/Status';
-import { Container } from './components/StyleProp/Container';
-import { Button } from './components/UltilComponents/Button';
-import { Input } from './components/UltilComponents/Input';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Main } from "./components/Main";
+import Person from "./components/Person";
+import { PersonList } from "./components/PersonList";
+import { Counter } from "./components/StateComponents/Counter";
+import { LoggedIn } from "./components/StateComponents/LoggedIn";
+import { Status } from "./components/Status";
+import { Container } from "./components/StyleProp/Container";
+import { Button } from "./components/UltilComponents/Button";
+import { Input } from "./components/UltilComponents/Input";
 
 function App() {
   const personName = {
-    first: 'Nguyen',
-    last: 'Anh Vu',
+    first: "Nguyen",
+    last: "Anh Vu",
   };
 
   const nameList = [
     {
-      first: 'Nguyen',
-      last: 'Van A',
+      first: "Nguyen",
+      last: "Van A",
     },
     {
-      first: 'Nguyen',
-      last: 'Van B',
+      first: "Nguyen",
+      last: "Van B",
     },
     {
-      first: 'Nguyen',
-      last: 'Van C',
+      first: "Nguyen",
+      last: "Van C",
     },
   ];
 
   return (
-    <div className='App'>
-      <Greet name={'Anhvu'} age={27} isLoggedIn />
+    <div className="App">
+      <Greet name={"Anhvu"} age={27} isLoggedIn />
       <Person name={personName} />
       {/* Array props */}
       <PersonList name={nameList} />
-      <Status status='success' />
+      <Status status="success" />
       {/* Advanced props */}
       <Main>
         <Heading>
@@ -47,41 +48,50 @@ function App() {
       {/* End Advanced props */}
 
       {/* Event props */}
-      <div className='event-props'>
+      <div className="event-props">
         <p>
           ----------------------------------------------------------------------------
         </p>
-        <Button handleClick={(event) => console.log('Button clicked', event)} />
-        <Input value='a' />
+        <Button handleClick={(event) => console.log("Button clicked", event)} />
+        <Input value="a" />
       </div>
       {/* End Advanced props */}
 
       {/* Style props */}
-      <div className='style-props'>
+      <div className="style-props">
         <p>
           ----------------------------------------------------------------------------
         </p>
         <Container
           styles={{
-            border: '1px solid pink',
-            padding: '1rem',
-            width: '20%',
-            backgroundColor: 'pink',
-            color: '#fff',
-            margin: '0 auto',
+            border: "1px solid pink",
+            padding: "1rem",
+            width: "20%",
+            backgroundColor: "pink",
+            color: "#fff",
+            margin: "0 auto",
           }}
         />
       </div>
       {/* End Style props */}
 
       {/* Handle State with useState */}
-      <div className='state'>
+      <div className="state">
         <p>
           ----------------------------------------------------------------------------
         </p>
         <LoggedIn />
       </div>
       {/* End Handle State with useState */}
+
+      {/* Handle state with useReducer */}
+      <div className="reducer">
+        <p>
+          ----------------------------------------------------------------------------
+        </p>
+        <Counter />
+      </div>
+      {/* End handle state with useReducer */}
     </div>
   );
 }
---
+++ src/components/StateComponents/Counter.tsx
@@ -0,0 +1,47 @@
+import React, { useReducer } from "react";
+
+type CounterState = {
+  count: number;
+};
+
+type CountAction = {
+  type: string;
+  payload: number;
+};
+
+const initialState = { count: 0 };
+
+function reducer(state: CounterState, action: CountAction) {
+  switch (action.type) {
+    case "increment":
+      return {
+        count: state.count + action.payload,
+      };
+
+    case "decrement":
+      return {
+        count: state.count - action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      Count: {state.count}
+      <br />
+      <div className="btn-login">
+        <button onClick={() => dispatch({ type: "increment", payload: 10 })}>
+          Increment 10
+        </button>
+        <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
+          Decrement 10
+        </button>
+      </div>
+    </div>
+  );
+};
